e51cc5684dcd18c855fd6311a4e46a5d6ce1da06
vizotic <vizotic@gmail.com>
2018-10-05 14:39:27 +0545

    Added home navigation, home component, and created side navs and nav tabs

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
-    imports: [MatBadgeModule, MatButtonModule],
-    exports: [MatBadgeModule, MatButtonModule
+    imports: [MatBadgeModule,
+         MatButtonModule,
+         MatSidenavModule,
+         MatToolbarModule,
+         MatIconModule,
+         MatBadgeModule,
+         MatListModule,
+         MatTabsModule
+        ],
 
-    ]
+    exports: [MatBadgeModule,
+         MatButtonModule,
+          MatSidenavModule,
+           MatToolbarModule,
+           MatIconModule,
+           MatBadgeModule,
+           MatListModule,
+           MatTabsModule
+        ]
 })
 
 export class MaterialModule {
